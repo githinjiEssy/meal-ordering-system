98b599e0e8c6c779ecb1514829f1a49ce9c80341
githinjiEssy <esthergithinji49@gmail.com>
2024-11-10 11:23:53 +0300

    header top

--- client/src/Componets/Home page/Header.jsx
+++ client/src/Componets/Home page/Header.jsx
@@ -4,25 +4,28 @@ import { FaMapLocationDot } from "react-icons/fa6";
 import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { TiShoppingCart } from "react-icons/ti";
+import "./Header.css";
 
 function Header() {
   return (
-    <div className='hader-container'>
-        <div className="header_top">
-            <div className="delivery">
-                <TbTruckDelivery/>
-                <h3>Free delivery around campus</h3>
-            </div>
+    <div className='header-container'>
+        <div className="header-wrapper">
+            <div className="header_top">
+                <div className="delivery">
+                    <TbTruckDelivery className="header_top-icon"/>
+                    <h3>Free delivery around campus</h3>
+                </div>
 
-            <div className="location">
-                <FaMapLocationDot/>
-                <h3>Sironi Restaurant USIU-Africa</h3>
-            </div>
+                <div className="location">
+                    <FaMapLocationDot className="header_top-icon"/>
+                    <h3>Sironi Restaurant USIU-Africa</h3>
+                </div>
 
-            <div className="header_socials">
-                <FaFacebookSquare/>
-                <FaInstagramSquare/>
-                <FaSquareXTwitter />
+                <div className="header_socials">
+                    <FaFacebookSquare className="header_top-icon"/>
+                    <FaInstagramSquare className="header_top-icon"/>
+                    <FaSquareXTwitter className="header_top-icon"/>
+                </div>
             </div>
         </div>
 
@@ -34,15 +37,9 @@ function Header() {
 
             <div className="nav_links">
                 <ul className="header_links">
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">menu</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact Us</a>
-                    </li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Contact Us</a></li>
                 </ul>
             </div>
 
@@ -50,10 +47,8 @@ function Header() {
                 <div className="cart_icon">
                     <TiShoppingCart />
                 </div>
-
                 <button>SignIn</button>
             </div>
-
         </div>
 
         <div className="header_content">
@@ -64,16 +59,18 @@ function Header() {
                 <div className="empty"></div>
                 <div className="empty"></div>
 
-                <h1 className="header_tile">Make an order of your favorite food</h1>
+                <h1 className="header_title">Make an order of your favorite food</h1>
                 <p>Save time by ordering your favorite food from anywhere around campus</p>
             </div>
 
             <div className="content_right">
-                
+                <img src="" alt="" />
+                <div className="empty_left"></div>
+                <img src="" alt="" />
             </div>
         </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
