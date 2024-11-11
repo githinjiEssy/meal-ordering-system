a344fc7b4c19c6e2abb09fc10614b5dc67a7e990
githinjiEssy <esthergithinji49@gmail.com>
2024-11-11 14:10:52 +0300

    added image hover effect and about section

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Componets/Home page/Header';
+import About from './Componets/Home page/About';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <About/>
       </div>
     );
   }
